162c7ed2ff31df7d5db0c9d19895df7529cdcd5e
穗乃果 <1763575579@qq.com>
2025-06-17 11:52:12 +0800

    update marne version

--- BF1MarneTools/Properties/AssemblyInfo.cs
+++ BF1MarneTools/Properties/AssemblyInfo.cs
@@ -14,9 +14,9 @@
 [assembly: AssemblyTitle("战地1马恩工具箱")]
 [assembly: AssemblyCompany("https://battlefield.vip")]
 [assembly: AssemblyProduct("战地1马恩工具箱")]
-[assembly: AssemblyVersion("1.4.0.0")]
-[assembly: AssemblyFileVersion("1.4.0.0")]
-[assembly: AssemblyInformationalVersion("1.4.0.0")]
+[assembly: AssemblyVersion("1.4.0.1")]
+[assembly: AssemblyFileVersion("1.4.0.1")]
+[assembly: AssemblyInformationalVersion("1.4.0.1")]
 
 // 不重复
 [assembly: AssemblyDescription("https://battlefield.vip")]
